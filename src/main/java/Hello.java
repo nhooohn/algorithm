0656cb4f02aad0e1b48f59845b46c55f2eea0aaf
nhong <1147881164@qq.com>
2023-05-27 15:06:35 +0800

    update main first

---
+++ src/main/java/Hello.java
@@ -0,0 +1,8 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("3"+33);
+        System.out.println(3+"3"+3);
+        System.out.println(3+3+"3");
+    }
+}
